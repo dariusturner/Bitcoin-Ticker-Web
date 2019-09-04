64976220dfa3676639a7d60f103fbb0fd6a78a66
Darius Turner <dariusot731@gmail.com>
2019-09-04 08:21:55 -0400

    Add functionality to use bitcoinaverage.com's API to get data - Complete

---
+++ index.js
@@ -0,0 +1,38 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  var crypto = req.body.crypto;
+  var fiat = req.body.fiat;
+
+  baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+  finalURL = baseURL + crypto + fiat;
+
+  request(finalURL, function(error, response, body){
+    var data = JSON.parse(body);
+    var price = data.last;
+    var currentDate = data.display_timestamp;
+
+    res.write("<p>The current date is " + currentDate + "</p>");
+
+    res.write("<h1>The price of " + crypto + " is " + price + " " + fiat + "</h1>");
+
+    res.send();
+    
+  });
+});
+
+app.listen(port, function(){
+  console.log("Server is running on port " + port + ".");
+});
